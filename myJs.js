a7720807720f15b6a8a4abbcfa61532099afc7e1
Minhaza <minhdqfs90805@fpt.edu.vn>
2021-12-20 12:01:14 +0700

    Big Update
    Finishing Section 8, Footer

--- myJs.js
+++ myJs.js
@@ -5,22 +5,16 @@ console.log('script.js says "I\'m here"');
 function openMenu() {
     document.getElementById("menu").style.width = "100%";
     document.getElementById("logo").style.opacity = "0%";
-    document.getElementById("arrowLeft").style.opacity = "0%";
-    document.getElementById("arrowRight").style.opacity = "0%";
+
 
 }
 function closeMenu() {
     document.getElementById("menu").style.width = "0%";
     document.getElementById("logo").style.opacity = "100%";
-    document.getElementById("arrowLeft").style.opacity = "100%";
-    document.getElementById("arrowRight").style.opacity = "100%";
+
 }
 
 var slideIndex = 1;
-showSlides(1, 0);
-
-
-
 
 // This is changing background
 
@@ -34,20 +28,9 @@ function currentSlide(n) {
     showSlides(slideIndex = n);
 }
 
-// function showSlides(n, no) {
-//     var i;
-//     var x = document.getElementsByClassName(slideId[no]);
-//     if (n > x.length) {slideIndex[no] = 1}    
-//     if (n < 1) {slideIndex[no] = x.length}
-//     for (i = 0; i < x.length; i++) {
-//        x[i].style.display = "none";  
-//     }
-//     x[slideIndex[no]-1].style.display = "block";  
-//   }
-
 function showSlides(n) {
     var i;
-    var slides = document.getElementsByClassName("slides");
+    var slides = document.getElementsByClassName("mySlides1");
     var dots = document.getElementsByClassName("dot");
 
     if (n > slides.length) { slideIndex = 1 }
@@ -55,7 +38,7 @@ function showSlides(n) {
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
-    slides[slideIndex - 1].style.display = "block";
+
     for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
     }
@@ -64,7 +47,37 @@ function showSlides(n) {
 }
 
 
-// This is hiding navbar 
+
+// var slideIndex2 = 1;
+
+// function plusSlides2(n2) {
+//   showSlides2(slideIndex2 += n2);
+// }
+
+// function currentSlide2(n2) {
+//   showSlides2(slideIndex2 = n2);
+// }
+
+// function showSlides2(n2) {
+//   var j;
+//   var slides2 = document.getElementsByClassName("mySlides2");
+//   var dots2 = document.getElementsByClassName("gallery");
+//   var captionText = document.getElementById("caption");
+//   if (n2 > slides2.length) {slideIndex2 = 1}
+//   if (n2 < 1) {slideIndex2 = slides2.length}
+//   for (j = 0; j < slides2.length; j++) {
+//       slides2[j].style.display = "none";
+//   }
+//   for (j = 0; j < dots2.length; j++) {
+//       dots2[j].className = dots2[j].className.replace(" active", "");
+//   }
+//   slides2[slideIndex2 - 1].style.display = "block";
+//   dots2[slideIndex2 - 1].className += " active";
+//   captionText.innerHTML = dots2[slideIndex2 - 1].alt;
+// }
+
+
+// // This is hiding navbar 
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
 var currentScrollPos = window.pageYOffset;
